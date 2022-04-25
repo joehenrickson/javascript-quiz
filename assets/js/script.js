c4c0cb0a38476f76d4f62dec66479d0820848117
joehenrickson <joehenrickson@gmail.com>
2022-04-24 19:35:11 -0500

    new html and start of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,59 @@
 
+var start = document.querySelector("#start");
+var quiz = document.querySelector("#quiz");
+var display = document.querySelector('#time');
+var question = document.querySelector("#question");
+var answerA = document.querySelector("#A");
+var answerB = document.querySelector("#B");
+var answerC = document.querySelector("#C");
+var answerD = document.querySelector("#D");
+var scoreDiv = document.querySelector("#scoreContainer");
+var leaderList = document.querySelector("#leaderboards");
+var restart = document.querySelector("#restart");
+var leaderboard = [];
+var beginning;
 
 var questions = [{
     question: "Commonly used datatypes DO NOT include _______.", 
-    answers: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
-    correctAnswer: "C. Alerts"
+    answerA : "Strings", 
+    answerB : "Booleans", 
+    answerC : "Alerts", 
+    answerD : "Numbers",
+    correct : "C"
 },
 {
     question: "The condition in an if/else statement is enclosed with __________.",
-    answers: ["A. Quotes", "B. Curly Brackets", "C. Parenthesis", "D. Square Brackets"],
-    correctAnswer: "C. Parenthesis"
+    answerA : "Quotes", 
+    answerB : "Curly Brackets", 
+    answerC : "Parenthesis", 
+    answerD : "Square Brackets",
+    correct : "C"
 },
 {
     question: "Arrays in Javascript can be used to store ________.",
-    answers: ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"],
-    correctAnswer: "D. All of the Above"
+    answerA : "Numbers and Strings", 
+    answerB : "Other Arrays", 
+    answerC : "Booleans", 
+    answerD : "All of the Above",
+    correct : "D"
 },
 {
     question: "String values must be enclosed within __________ when being assigned to variables.",
-    answers: ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parenthesis"],
-    correctAnswer: "C. Quotes"
+    answerA : "Commas", 
+    answerB : "Curly Brackets",
+    answerC : "Quotes", 
+    answerD : "Parenthesis",
+    correct : "C"
 },
 {
     question: "A very useful tool used during development and debugging for printing content to the debugger is _________",
-    answers: ["A. Javascript", "B. Terminal/Bash", "C. for loops", "D. console log"],
-    correctAnswer: "D. console log"
+    answerA : "Javascript", 
+    answerB : "Terminal/Bash", 
+    answerC : "for loops", 
+    answerD : "console log",
+    correct : "D" 
 }
 ];
-
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = "Time: " + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-document.getElementById("pull").onclick = function () {
-    document.getElementById("remove").remove();
-
-    var div = document.createElement('div');
-    div.classList.add('question')
-       div.innerHTML = "<h1>Commonly used data types DO NOT include...</h1>" 
-       document.getElementsByTagName('body')[0].appendChild(div);
-
-    var ul = document.createElement('ul');
-    ul.classList.add('multChoice')
-    ul.innerHTML = 
-    "<li>Strings</li><li>Booleans</li><li>Alerts</li><li>Numbers</li>"
-    document.getElementsByTagName('div')[0].appendChild(ul);
-    var sixtySeconds = 60,
-        display = document.querySelector('#timer');
-    startTimer(sixtySeconds, display);
-
-};
 
 
 
